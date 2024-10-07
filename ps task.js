ead39b91e39f6622b86854a3662cfd450e3d0f5e
NaveenYendluru <naveenyendluru@gmail.com>
2024-10-07 10:10:43 +0530

    save

---
+++ ps task.js
@@ -0,0 +1,41 @@
+
+function bigger(a,b,c){
+    let ans=(a>b && a>c)?"a is bigger":(b>a && b>c)?"b is bigger":"c is bigger";
+    console.log(ans);
+    }
+    bigger(1,13,8);
+
+
+    for(let i=1;!(i>10);i++){
+        console.log(i);
+        }
+
+
+        for(let i=10;!(i<1);i--){
+            console.log(i);
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
